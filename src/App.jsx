b6dd4ac7b05eea1246b2b9cc72e89d5c584b30eb
sali taymour <saal.saal99@gmail.com>
2022-06-23 00:36:19 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-
 import "./App.scss";
 import { Noun } from "./components/Noun";
-import {Book} from "./components/Book";
+import { Book } from "./components/Book";
 import { TechPerson } from "./components/TechPerson";
 import { Setting } from "./components/Setting";
 import { Employee } from "./components/Employee";
@@ -20,9 +19,9 @@ const url = `${backend_url}/all`;
 console.log(backend_url);
 const separator = "|";
 function App() {
-    // const [siteData, setSiteData] = useState({});
-    //search items:
-    /*[{
+  // const [siteData, setSiteData] = useState({});
+  //search items:
+  /*[{
   kind:"noun",
   bulkSearch:"Nutiz",
   item:{
@@ -31,54 +30,50 @@ function App() {
     'plural':'die Notizen',
   },
 }]*/
-    const [searchItems, setSearchItems] = useState([]);
-    const [filteredSearchItems, setFilteredSearchItems] = useState([]);
+  const [searchItems, setSearchItems] = useState([]);
+  const [filteredSearchItems, setFilteredSearchItems] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            const _siteData = (await axios.get(url)).data;
-            // setSiteData(_siteData);
-            // console.log(_siteData)
-            const _searchItems = [];
-            _siteData.nouns.forEach((item) => {
-                _searchItems.push({
-                    kind: "noun",
-                    bulkSearch: item.singular,
-                    item,
-                });
-            });
+  useEffect(() => {
+    (async () => {
+      const _siteData = (await axios.get(url)).data;
+      // setSiteData(_siteData);
+      // console.log(_siteData)
+      const _searchItems = [];
+      _siteData.nouns.forEach((item) => {
+        _searchItems.push({
+          kind: "noun",
+          bulkSearch: item.singular,
+          item,
+        });
+      });
 
-            _siteData.books.forEach((item) => {
-                _searchItems.push({
-                    kind: "book",
-                    bulkSearch: item.title + separator + item.description,
-                    item,
-                });
-            });
-            _siteData.techPersons.forEach((item) => {
-                _searchItems.push({
-                    kind: "techPerson",
-                    bulkSearch:
-                        item.fullName +
-                        separator +
-                        item.quickInfo +
-                        separator +
-                        item.body,
-                    item,
-                });
-            });
-            
-            _siteData.employees.forEach((item) => {
-                _searchItems.push({
-                    kind: "employee",
-                    bulkSearch: item.FIRST_NAME + separator + item.LAST_NAME,
-                    item,
-                });
-            });
-_siteData.translations.forEach((item) => {
-    _searchItems.push({
-        kind: "translation",
-        bulkSearch:
+      _siteData.books.forEach((item) => {
+        _searchItems.push({
+          kind: "book",
+          bulkSearch: item.title + separator + item.description,
+          item,
+        });
+      });
+      _siteData.techPersons.forEach((item) => {
+        _searchItems.push({
+          kind: "techPerson",
+          bulkSearch:
+            item.fullName + separator + item.quickInfo + separator + item.body,
+          item,
+        });
+      });
+
+      _siteData.employees.forEach((item) => {
+        _searchItems.push({
+          kind: "employee",
+          bulkSearch: item.FIRST_NAME + separator + item.LAST_NAME,
+          item,
+        });
+      });
+      _siteData.translations.forEach((item) => {
+        _searchItems.push({
+          kind: "translation",
+          bulkSearch:
             item.fromLanguage +
             separator +
             item.toLanguage +
@@ -86,99 +81,82 @@ _siteData.translations.forEach((item) => {
             item.fromPhrase +
             separator +
             item.toPhrase,
-        item,
-    });
-});
+          item,
+        });
+      });
 
-_siteData.jobs.forEach((item) => {
-    _searchItems.push({
-        kind: "job",
-        bulkSearch: item.html,
-        item,
-    });
-});
-_siteData.landscapePhotos.forEach((item) => {
-    _searchItems.push({
-        kind: "landscapePhoto",
-        bulkSearch: item,
-        item,
-    });
-});
+      _siteData.jobs.forEach((item) => {
+        _searchItems.push({
+          kind: "job",
+          bulkSearch: item.html,
+          item,
+        });
+      });
+      _siteData.landscapePhotos.forEach((item) => {
+        _searchItems.push({
+          kind: "landscapePhoto",
+          bulkSearch: item,
+          item,
+        });
+      });
 
-            setSearchItems(_searchItems);
-            setFilteredSearchItems([]);
-        })();
-    }, []);
-    const handleSearch = (e) => {
-        const searchText = e.target.value.trim();
-		let _filteredSearchItems = [];
-		if (searchText.length >= 3) {
-			_filteredSearchItems = searchItems.filter((m) =>
-				m.bulkSearch.toLowerCase().includes(searchText.toLowerCase())
-			);
-			setFilteredSearchItems(_filteredSearchItems);
-		}
-		if (searchText === '') {
-			_filteredSearchItems = [];
-			setFilteredSearchItems(_filteredSearchItems);
-		}
-    };
-    //wir sollen in backend erlauben dass unsere front End darauf kommen darf before wir uberhaubt anything displaying
-    //cors origin problem
+      setSearchItems(_searchItems);
+      setFilteredSearchItems([]);
+    })();
+  }, []);
+  const handleSearch = (e) => {
+    const searchText = e.target.value.trim();
+    let _filteredSearchItems = [];
+    if (searchText.length >= 3) {
+      _filteredSearchItems = searchItems.filter((m) =>
+        m.bulkSearch.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setFilteredSearchItems(_filteredSearchItems);
+    }
+    if (searchText === "") {
+      _filteredSearchItems = [];
+      setFilteredSearchItems(_filteredSearchItems);
+    }
+  };
+  //wir sollen in backend erlauben dass unsere front End darauf kommen darf before wir uberhaubt anything displaying
+  //cors origin problem
 
-    return (
-        <div className="App">
-            <div>Search Info</div>
-            {Object.keys(searchItems).length === 0 ? (
-                <div>Loading...</div>
-            ) : (
-                <>
-                    {/* <div>There are {searchItems.length} items</div> */}
-                    <input
-                        type="text"
-                        autoFocus
-                        onChange={(e) => handleSearch(e)}
-                    />
+  return (
+    <div className="App">
+      <div>Search Info</div>
+      {Object.keys(searchItems).length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {/* <div>There are {searchItems.length} items</div> */}
+          <input type="text" autoFocus onChange={(e) => handleSearch(e)} />
 
-                    <div className="searchItems">
-                        {filteredSearchItems.map((item, i) => {
-                            return (
-                                <React.Fragment key={i}>
-                                    {item.kind === "noun" && (
-                                        <Noun item={item.item} />
-                                    )}
-                                    {item.kind === "book" && (
-                                        <Book item={item.item} />
-                                    )}
-                                    {item.kind === "techPerson" && (
-                                        <TechPerson item={item.item} />
-                                    )}
-                                    {item.kind === "setting" && (
-                                        <Setting item={item.item} />
-                                    )}
-                                    {item.kind === "employee" && (
-                                        <Employee item={item.item} />
-                                    )}
-                                    {item.kind === "translation" && (
-                                        <Translation item={item.item} />
-                                    )}
-                                    {item.kind === "job" && (
-                                        <Job item={item.item} />
-                                    )}
-                                    {item.kind === "landscapePhoto" && (
-                                        <LandscapePhoto
-                                            item={item.item}
-                                            baseUrl={baseUrl}
-                                        />
-                                    )}
-                           </React.Fragment>
-                            );
-                        })}
-                    </div>
-                </>
-            )}
-        </div>
-    );
+          <div className="searchItems">
+            {filteredSearchItems.map((item, i) => {
+              return (
+                <React.Fragment key={i}>
+                  {item.kind === "noun" && <Noun item={item.item} />}
+                  {item.kind === "book" && <Book item={item.item} />}
+                  {item.kind === "techPerson" && (
+                    <TechPerson item={item.item} />
+                  )}
+                  {item.kind === "setting" && <Setting item={item.item} />}
+                  {item.kind === "employee" && <Employee item={item.item} />}
+                  {item.kind === "translation" && (
+                    <Translation item={item.item} />
+                  )}
+                  {item.kind === "job" && <Job item={item.item} />}
+                  {item.kind === "landscapePhoto" && (
+                    <LandscapePhoto item={item.item} baseUrl={baseUrl} />
+                  )}
+                </React.Fragment>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
